634169193d46a0aa3352adf53fa0988102971b90
TDYe123 <18365225454@163.com>
2019-04-16 00:36:07 +0800

    修改数据库异步操作

--- server/routes/query.js
+++ server/routes/query.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')()
 const controllers = require('require-all')({
-	dirname: 'D:/MyProjects/FreshFoodSuperMarket/controllers' 
+	dirname: 'D:/MyProjects/FreshFoodSuperMarket/server/controllers' 
 })
 
 router.prefix('/query')

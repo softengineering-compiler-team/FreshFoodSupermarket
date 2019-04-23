af57b43cdbb45fa1c870832fdbf297049e0b894a
EthanZheng19980221 <49140316+EthanZheng19980221@users.noreply.github.com>
2019-04-23 09:46:53 +0800

    Update admin.js
    显示订单

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -2,71 +2,49 @@ const db = require('../utils/db.js')
 
 async function order(ctx, next) {
 	let sql = `select  receive.*, goods.goodsName from receive inner join goods on receive.goodsNo = goods.goodsNo where status = 0 order by orderTime desc, orderNo desc`
-<<<<<<< HEAD
 	//console.log(sql);
 	let data = await db(sql)
-	//console.log(data)
-
+	var totalData = []
 	let orderNo = data[0].orderNo
-
 	var orderNum = new Array()
 	orderNum.push(orderNo)
 
-	var numData = new Array()
-	var goodsData = new Array() 
-	var orderData = new Array()
-	var totalOrder = new Array()
-	
 	for(let i=0;i<data.length;i++){
-		temporder = data[i].orderNo
+		let temporder = data[i].orderNo
 		if(temporder != orderNo){
 			orderNum.push(temporder)
 			orderNo = temporder
 		}
 	}
-
 	for(let i=0;i<orderNum.length;i++){
 		let temporder = orderNum[i]
+		console.log(temporder)
 		let sql1 = `SELECT  receive.*, goods.goodsName FROM receive ,goods WHERE receive.goodsNo = goods.goodsNo AND STATUS = 0 AND orderNo = '${temporder}'`
 		let data1 = await db(sql1)
-		var goodsData = new Array() 
-		var numData = new Array()
-		var orderData = new Array()
+		var goodsList = new Array()
+		var goodsObj = {}
+		var orderData = {}
 		let total = 0
-		orderData.push(data1[i].orderNo)
+		orderData['orderNo'] = data1[i].orderNo
 		for(let j=0;j<data1.length;j++){
-			goodsData.push(data1[j].goodsName)
-			numData.push(data1[j].num)
-			total += data1[i].subtotal
+			goodsObj['goodsName'] = data1[j].goodsName
+			goodsObj['num'] = data1[j].num
+			goodsList.push(goodsObj)
+			total += data1[j].subtotal
 		}
-		orderData.push(goodsData)
-		orderData.push(numData)
-		orderData.push(total)
-		orderData.push(data1[i].orderTime)
-		orderData.push(data1[i].username)
-		orderData.push(data1[i].status)
-
-		totalOrder.push(orderData)
+		
+		orderData['goodsList'] = goodsList
+		orderData['total'] = total
+		orderData['orderTime'] = data1[i].orderTime
+		orderData['username'] = data1[i].username
+		orderData['status'] = data1[i].status
+		totalData.push(orderData)
 	}
-	
-	
-	console.log(totalOrder);
-
 	ctx.body = {
 		code: 0,
-		data: totalOrder
-=======
-	console.log(sql);
-	let data = await db(sql)
-	console.log(data)
-
-	ctx.body = {
-		code: 0,
-		data: data
->>>>>>> e7619beae4581c2a57c9787ab65f011d2f00b6ad
+		data: totalData
 	}
 }
-
 module.exports = {
 	order: order
 	

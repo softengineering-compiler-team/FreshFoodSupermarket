ea95b942b82f1e38bb77d58be26b70b278167a1c
EthanZheng19980221 <49140316+EthanZheng19980221@users.noreply.github.com>
2019-04-23 09:43:36 +0800

    Update config.js
    update

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,6 @@ module.exports = {
 	        'host': 'localhost',
 	        'port': '3306',
 	        'user': 'root',
-<<<<<<< HEAD
-	        'password': 'Ethan0221',
-=======
 	        'password': '158728',
->>>>>>> e7619beae4581c2a57c9787ab65f011d2f00b6ad
 	        'database': 'freshfood',
 	    }

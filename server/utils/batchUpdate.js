7be70ff08de02c84151461963abf64178b145430
TDYe123 <18365225454@163.com>
2019-05-02 22:10:03 +0800

    add:batchUpdate MySQL

--- server/utils/batchUpdate.js
+++ server/utils/batchUpdate.js
@@ -1,12 +1,30 @@
 const MySQL_db = require('../utils/db.js').MySQL_db
 
+/*性能极低，待改善*/
 async function batchUpdate(goodsList) {
 	for(let i=0; i<goodsList.length; i++) {
-		let sql = `update fav set goodsNo = goodsNo + ${goodsList[i].num} where username = '${goodsList[i].username}' and goodsNo = '${goodsList[i].goodsNo}`
+		let sql = `update fav set \`${goodsList[i].goodsNo}\` = \`${goodsList[i].goodsNo}\` + ${goodsList[i].num} where username = '${goodsList[i].username}'`
+		await MySQL_db(sql)
 	}
-	await MySQL_db(sql)
-	let data = ''
-	return data
+	return
 }
+
+/*测试数据*/
+/*
+let goodsList = [
+					{
+						username:  "叶田地",
+					 	goodsNo:  1,
+					 	num: 99,
+					 	subtotal: 990.22
+					}, {
+						username: "叶田地",
+						goodsNo: 2,
+						num: 98,
+						subtotal: 50
+					}]
+
+batchUpdate(goodsList)
+*/
 
 module.exports = batchUpdate

b62c8f916f5f2a0f31cb11e2be7cdc8be2b2e6b5
TDYe123 <18365225454@163.com>
2019-05-09 12:28:06 +0800

    -

--- server/controllers/goods.js
+++ server/controllers/goods.js
@@ -33,10 +33,10 @@ async function fruits(ctx, next) {
 async function vegetables(ctx, next) {
 	ctx.session.refresh()
 
-	var subtypeList = ['香蕉', '苹果', '葡萄', '西瓜', '梨', '柚子', '车厘子']
+	var subtypeList = ['茄子', '黄瓜', '小辣椒', '小白菜', '大葱', '毛豆', '番茄']
 	var data = new Array()
 	for(let subtype of subtypeList) {
-		let sql = `select * from sale where type = '水果' and subtype = '${subtype}' limit 5`
+		let sql = `select * from sale where type = '蔬菜' and subtype = '${subtype}' limit 5`
 		let sub_data = await db.MySQL_db(sql)
 		data.push(sub_data)
 	}
@@ -50,10 +50,10 @@ async function vegetables(ctx, next) {
 async function seafoods(ctx, next) {
 	ctx.session.refresh()
 
-	var subtypeList = ['香蕉', '苹果', '葡萄', '西瓜', '梨', '柚子', '车厘子']
+	var subtypeList = ['生蚝', '帝王蟹', '乌贼', '扇贝', '龙虾', '鱿鱼', '海胆']
 	var data = new Array()
 	for(let subtype of subtypeList) {
-		let sql = `select * from sale where type = '水果' and subtype = '${subtype}' limit 5`
+		let sql = `select * from sale where type = '海鲜' and subtype = '${subtype}' limit 5`
 		let sub_data = await db.MySQL_db(sql)
 		data.push(sub_data)
 	}
@@ -67,10 +67,10 @@ async function seafoods(ctx, next) {
 async function meat(ctx, next) {
 	ctx.session.refresh()
 
-	var subtypeList = ['香蕉', '苹果', '葡萄', '西瓜', '梨', '柚子', '车厘子']
+	var subtypeList = ['火鸡', '牛肉', '羊肉', '烤鸭', '鹅肉', '猪肉', '鸡肉']
 	var data = new Array()
 	for(let subtype of subtypeList) {
-		let sql = `select * from sale where type = '水果' and subtype = '${subtype}' limit 5`
+		let sql = `select * from sale where type = '肉类' and subtype = '${subtype}' limit 5`
 		let sub_data = await db.MySQL_db(sql)
 		data.push(sub_data)
 	}
@@ -84,10 +84,10 @@ async function meat(ctx, next) {
 async function dairy(ctx, next) {
 	ctx.session.refresh()
 	
-	var subtypeList = ['香蕉', '苹果', '葡萄', '西瓜', '梨', '柚子', '车厘子']
+	var subtypeList = ['奶酪', '奶油', '奶粉', '酸奶', '巴氏鲜奶', '炼乳', '冰淇淋']
 	var data = new Array()
 	for(let subtype of subtypeList) {
-		let sql = `select * from sale where type = '水果' and subtype = '${subtype}' limit 5`
+		let sql = `select * from sale where type = '乳制品' and subtype = '${subtype}' limit 5`
 		let sub_data = await db.MySQL_db(sql)
 		data.push(sub_data)
 	}

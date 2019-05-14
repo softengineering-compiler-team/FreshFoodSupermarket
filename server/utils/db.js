3f424ffbb383abdbe385e61c40d5dd2157b4d659
TDYe123 <18365225454@163.com>
2019-05-14 17:44:19 +0800

    add: Noe4j database

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,5 +1,6 @@
 const MySQL_config = require('../config/MySQL-config') 
 const Redis_config = require('../config/Redis-config') 
+const db = require('../config/Neo4j-config')
 const mysql = require('mysql'),
      	pool  = mysql.createPool(MySQL_config);
 const redis = require('ioredis'),
@@ -23,9 +24,23 @@ async function MySQL_db(sql) {
 	}) 	
 }
 
+
+async function Neo4j_db(cypher) {
+	return new Promise((resolve, reject) => {
+		db.cypherQuery(cypher, function(err, result) {
+		    if(err) { 
+		    	return console.error(err) 
+		    	reject(err)
+		    }
+		    resolve(result)
+		})
+	})
+}
+
 module.exports = {
 	MySQL_db: MySQL_db,
-	Redis_db: new redis(Redis_config)
+	Redis_db: new redis(Redis_config),
+	Neo4j_db: Neo4j_db
 }
 
 

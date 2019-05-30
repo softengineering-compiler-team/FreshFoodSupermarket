2ba6e83b7be0cdb32c41f7b8dcb9cb6102b93c0c
TDYe123 <18365225454@163.com>
2019-05-30 23:09:43 +0800

    订单分类并排序

--- server/Models/OrderModel.js
+++ server/Models/OrderModel.js
@@ -87,7 +87,7 @@ class OrderModel {
 		}
 		for(let i=0;i<orderNum.length;i++){
 			let temporder = orderNum[i]
-			let sql1 = `SELECT  receive.*, goods.goodsName FROM receive ,goods WHERE receive.goodsNo = goods.goodsNo AND orderNo = '${temporder}' and username ='${username}' order by orderTime desc`
+			let sql1 = `SELECT  receive.*, goods.goodsName FROM receive ,goods WHERE receive.goodsNo = goods.goodsNo AND orderNo = '${temporder}' and username ='${username}' order by status,orderTime desc`
 			let data1 = await MySQL_db(sql1)
 			var goodsList = new Array()
 			var orderData = {}
